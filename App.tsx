a18e7b753ce01c6b8ba866714913963d48380beb
aphil311 <aidanphillips21@outlook.com>
2022-08-20 00:27:45 -0400

    enable typescript

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Queue.</Text>
+      <Text>Join</Text>
+      <Text>Host</Text>
       <StatusBar style="auto" />
     </View>
   );
